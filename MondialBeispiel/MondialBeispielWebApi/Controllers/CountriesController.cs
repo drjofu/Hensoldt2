cead94486a86177a7cef0f9b74247aac00e76a88
drjofu <drjofu@web.de>
2022-12-02 11:46:49 +0100

    Tag 2

--- MondialBeispiel/MondialBeispielWebApi/Controllers/CountriesController.cs
+++ MondialBeispiel/MondialBeispielWebApi/Controllers/CountriesController.cs
@@ -9,18 +9,24 @@ namespace MondialBeispielWebApi.Controllers
   public class CountriesController : ControllerBase
   {
     private readonly World world;
+    private readonly ILogger<CountriesController> logger;
 
-    public CountriesController(World world)
+    public CountriesController(World world, ILogger<CountriesController> logger)
     {
       this.world = world;
+      this.logger = logger;
     }
 
     [HttpGet]
     public ActionResult<IEnumerable<Country>> Get(string continentId)
     {
+      logger.LogInformation("Es wurde nach den Länder für die Kontinent-Id {continentId} gefragt", continentId);
+
       var countries = world.GetCountriesOnContinent(continentId);
       if (countries.Count() > 0)
         return Ok(countries);
+
+      logger.LogError("Kontinent mit Id {continentId} gibt es nicht", continentId);
       return BadRequest($"Kontinent mit Id {continentId} gibt es nicht.");
     }
   }
---
+++ WeitereBeispiele/WeitereBeispiele/Controllers/SettingsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeitereBeispiele.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class SettingsController : ControllerBase
+  {
+    private readonly IConfiguration configuration;
+
+    public SettingsController(IConfiguration configuration)
+    {
+      this.configuration = configuration;
+    }
+
+    [HttpGet]
+    public string GetConfig()
+    {
+      var pfad = configuration["pfad"];
+      int anzahl = configuration.GetValue<int>("anzahl");
+      string passwort = configuration.GetValue<string>("passwort");
+
+      return $"pfad: {pfad}, Anzahl: {anzahl}, Pw: {passwort}";
+    }
+  }
+}

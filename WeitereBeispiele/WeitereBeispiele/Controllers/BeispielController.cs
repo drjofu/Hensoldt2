2c12b6b1b2098b6ec1f74b9da2ab81f2d8eaf7e1
drjofu <drjofu@web.de>
2022-12-02 13:02:26 +0100

    Tag 2

--- WeitereBeispiele/WeitereBeispiele/Controllers/BeispielController.cs
+++ WeitereBeispiele/WeitereBeispiele/Controllers/BeispielController.cs
@@ -7,5 +7,24 @@ namespace WeitereBeispiele.Controllers
   [ApiController]
   public class BeispielController : ControllerBase
   {
+    [HttpGet]
+    public async Task<ActionResult<string>> Get()
+    {
+      await Task.Delay(2000);
+      return "Hallo Welt";
+    }
+
+    [HttpGet("soNicht")]
+    public async void SoBitteNicht()
+    {
+      await Task.Delay(2000);
+      // ...
+    }
+
+    //public string Get2()
+    //{
+    //  Task.Delay(2000).Wait();
+    //  return "...";
+    //}
   }
 }

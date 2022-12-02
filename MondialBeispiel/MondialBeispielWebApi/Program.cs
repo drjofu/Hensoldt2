2da374726c518d08fc99f3a2a5d0952943dd3293
drjofu <drjofu@web.de>
2022-12-02 10:52:45 +0100

    Tag 2

--- MondialBeispiel/MondialBeispielWebApi/Program.cs
+++ MondialBeispiel/MondialBeispielWebApi/Program.cs
@@ -11,6 +11,13 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<World>();
 
+builder.Services.AddCors(setup=> 
+  setup.AddPolicy("CorsPolicy", 
+    cpb=> 
+       cpb.AllowAnyOrigin()
+          .AllowAnyHeader()
+          .AllowAnyMethod()));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,8 +27,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
 app.Run();
+
+//app.Run("http://localhost:10042");
 
